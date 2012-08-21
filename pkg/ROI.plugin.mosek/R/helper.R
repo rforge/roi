dc9ee200ffd3658eeab474f5eb4ea7eb20f4c320
stefan7th <stefan7th@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-21 12:50:56 +0000

    dummy package plugin.mosek based on plugin.cplex
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roi@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/ROI.plugin.mosek/R/helper.R
@@ -0,0 +1,25 @@
+## ROI.plugin.cplex: helper functions
+## Description: provides several helper functions
+
+## UTILITIES
+.as_Rcplex_sense <- function(x) {
+  TABLE <- c("L", "L", "G", "G", "E")
+  names(TABLE) <- c("<", "<=", ">", ">=", "==")
+  TABLE[x]
+}
+
+.all_zero_in_simple_triplet_matrix_or_NULL <- function(x){
+  if( is.null(x) )
+    return(TRUE)
+  stm <- as.simple_triplet_matrix(x)
+  if( ! length(stm$v) )
+    return(TRUE)
+  FALSE
+}
+
+.make_list_of_linear_constraints <- function(x){
+  if( is.null(x) )
+    return(list(NULL))
+  stm <- as.simple_triplet_matrix(x)
+  apply(x, 1, function(x) as.simple_triplet_matrix(matrix(x, nrow = 1)))
+}
