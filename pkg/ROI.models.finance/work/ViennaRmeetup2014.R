73a3104dde5ebc1e0254340b76150739657d2186
stefan7th <stefan7th@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-06-12 13:05:09 +0000

    R code complete
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roi@82 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ROI.models.finance/work/ViennaRmeetup2014.R
+++ pkg/ROI.models.finance/work/ViennaRmeetup2014.R
@@ -20,6 +20,9 @@
 ?nlm
 ?optimize
 
+## for a recent discussion on these solvers, see http://www.jstatsoft.org/v43/i09/paper
+
+
 ## Example 1)
 ## Nonlinear optimization of the function from the slides
 
@@ -41,23 +44,17 @@ fun <- function(x){
     1/3 * exp(-(x[1]+1)^2 - x[2]^2)
 }
 
-## TODO just call optim on this stuff
-
-
+## find optimum using the "Nelder-Mead" algorithm
+start <- c( 0, 0 )
+optim( start, fun, method = "Nelder-Mead" )
 
 ## optimization using a multistart procedure
 start <- list( c(0, 0), c(-1, -1), c(0, -1), c(0, 1) )
-seqt <- system.time(sol <- lapply(start, function(par) optim(par, fun, method = "Nelder-Mead",
-          lower = -Inf, upper = Inf, control = list(maxit = 1000000, beta = 0.01, reltol = 1e-15)))
-        )["elapsed"]
-seqt
+sol <- lapply( start, function(par)
+               optim(par, fun, method = "Nelder-Mead",
+                     control = list(maxit = 1000000, beta = 0.01, reltol = 1e-15)) )
 
-require(parallel)
-part <- system.time(sol <- mclapply(start, function(par)
-                      optim(par, fun, method = "Nelder-Mead", lower = -Inf, upper = Inf,
-                            control = list(maxit = 1000000, beta = 0.01, reltol = 1e-15)), mc.cores = 2)
-                    )["elapsed"]
-
+sapply( sol, function(x) x$value )
 
 
 ## Example 2a)
@@ -68,12 +65,27 @@ part <- system.time(sol <- mclapply(start, function(par)
 ## from optim() man page
 eurodistmat <- as.matrix(eurodist)
 
+## calculate coordinates using the distance matrix
+loc <- -cmdscale(eurodist, add = TRUE)$points
+x <- loc[,1]; y <- loc[,2]
+## rotate for conventional orientation
+plot(x, y, type = "n", asp = 1, xlab = "", ylab = "",
+     main = "Cities in Europe", axes = FALSE)
+text(x, y, labels(eurodist), cex = 0.8)
+
+## calculate distance
 distance <- function(sq) {  # Target function
     sq2 <- embed(sq, 2)
     sum(eurodistmat[cbind(sq2[,2], sq2[,1])])
 }
 
-genseq <- function(sq) {  # Generate new candidate sequence
+sq <- c(1:nrow(eurodistmat), 1)  # Initial sequence: alphabetic
+distance(sq)
+
+## for using SANN optimizer we need a function to generate a new
+## candidate sequence in order to solve a combinatorial optimization
+## problem
+genseq <- function(sq) {
     idx <- seq(2, NROW(eurodistmat)-1)
     changepoints <- sample(idx, size = 2, replace = FALSE)
     tmp <- sq[changepoints[1]]
@@ -82,36 +94,25 @@ genseq <- function(sq) {  # Generate new candidate sequence
     sq
 }
 
-sq <- c(1:nrow(eurodistmat), 1)  # Initial sequence: alphabetic
-distance(sq)
-                                        # rotate for conventional orientation
-loc <- -cmdscale(eurodist, add = TRUE)$points
-x <- loc[,1]; y <- loc[,2]
-s <- seq_len(nrow(eurodistmat))
-tspinit <- loc[sq,]
-
-plot(x, y, type = "n", asp = 1, xlab = "", ylab = "",
-     main = "initial solution of traveling salesman problem", axes = FALSE)
-arrows(tspinit[s,1], tspinit[s,2], tspinit[s+1,1], tspinit[s+1,2],
-       angle = 10, col = "green")
-text(x, y, labels(eurodist), cex = 0.8)
-
+## start the optimizer (simulated annealing)
 set.seed(123) # chosen to get a good soln relatively quickly
 res <- optim(sq, distance, genseq, method = "SANN",
              control = list(maxit = 30000, temp = 2000, trace = TRUE,
              REPORT = 500))
-res  # Near optimum distance around 12842
+res
 
-tspres <- loc[res$par,]
+## plot the optimal route
 plot(x, y, type = "n", asp = 1, xlab = "", ylab = "",
      main = "optim() 'solving' traveling salesman problem", axes = FALSE)
+tspres <- loc[res$par,]
+s <- seq_len(nrow(eurodistmat))
 arrows(tspres[s,1], tspres[s,2], tspres[s+1,1], tspres[s+1,2],
        angle = 10, col = "red")
 text(x, y, labels(eurodist), cex = 0.8)
 
 
 
-## Contributed packages: CRAN Task View Optimization 
+## Contributed packages: CRAN Task View Optimization
 ## and Mathematical Programming
 ## http://CRAN.R-Project.org/view=Optimization
 
@@ -138,10 +139,29 @@ tour_lengths <- c( sapply(tours, FUN = attr, "tour_length") )
 
 dotchart( tour_lengths/min(tour_lengths)*100 - 100, xlab = 'percent excess over "optimum"' )
 
+
 ## Example 3)
 ## (Mixed Integer) Linear Programming Example
+
 require( "Rglpk" )
 ?Rglpk_solve_LP
+
+## Simple mixed integer linear program.
+## maximize:    3 x_1 + 1 x_2 + 3 x_3
+## subject to: -1 x_1 + 2 x_2 +   x_3 <= 4
+##                      4 x_2 - 3 x_3 <= 2
+##                x_1 - 3 x_2 + 2 x_3 <= 3
+##                x_1, x_3 are non-negative integers
+##                x_2 is a non-negative real number
+
+obj <- c(3, 1, 3)
+mat <- matrix(c(-1, 0, 1, 2, 4, -3, 1, -3, 2), nrow = 3)
+dir <- c("<=", "<=", "<=")
+rhs <- c(4, 2, 3)
+types <- c("I", "C", "I")
+max <- TRUE
+
+Rglpk_solve_LP(obj, mat, dir, rhs, types = types, max = max)
 
 
 
@@ -164,19 +184,31 @@ create_L1_problem <- function(x, j) {
     len <- 1 + ncol(x) + 2 * nrow(x)
     beta <- rep(0, len)
     beta[j + 1] <- 1
-    OP(L_objective(c(rep(0, ncol(x) + 1), rep(1, 2 * nrow(x)))),
+    OP(## objective
+        L_objective(c(rep(0, ncol(x) + 1), rep(1, 2 * nrow(x)))),
+       ## constraints
        rbind(L_constraint(cbind(1, as.matrix(x), diag(nrow(x)),
-                                -diag(nrow(x))), rep("==", nrow(x)), rep(0, nrow(x))),
-             L_constraint(beta, "==", -1)),
+                                -diag(nrow(x))), rep("==", nrow(x)), rep(0, nrow(x))), # (1)
+             L_constraint(beta, "==", -1)),                                            # (2)
        bounds = V_bound(li = seq_len(ncol(x) + 1), ui = seq_len(ncol(x) + 1),
                         lb = rep(-Inf, ncol(x) + 1), ub = rep(Inf, ncol(x) + 1), nobj = len))
 }
 
 ## solve the optimization object LP
-ROI_solve( create_L1_problem(stackloss, 4), solver = "glpk" )$solution
+L1 <- create_L1_problem(stackloss, 4)
+ROI_solve( L1, solver = "glpk" )$solution
 
 ## compare to regression model implemented in package quantreg
 rq( stack.loss ~ stack.x, 0.5 ) # median (l1) regression  fit for the stackloss data
+
+## which other solvers are installed
+ROI_installed_solvers()
+
+## currently it is not not straightforward to find a proper solver for
+## solving a given problem
+names( ROI:::get_solver_methods(ROI:::OP_signature(op)) )
+
+ROI_solve( L1, solver = "symphony" )$solution
 
 
 
@@ -197,6 +229,7 @@ tail( US30 )
 par( mfrow = c(2,1) )
 plot( US30[, "IBM"], main = "IBM" )
 plot( US30[, "JPM"], main = "JPMorgan Chase" )
+par( mfrow = c(1,1) )
 
 ## need some helper functions for financial time series
 require( "TTR" )
@@ -224,6 +257,7 @@ ew <- c( sqrt(w %*% S %*% w),
 points( x = ew[1], y = ew[2], col = "orange" )
 text( x = ew[1], y = ew[2], labels = "EW", pos = 3, col = "orange" )
 
+
 ## Example 6)
 ## MV Optimization using ROI (long-only, fully invested)
 
@@ -232,9 +266,9 @@ full_invest <- L_constraint( rep( 1, ncol(r)), "==", 1 )
 long_only <- L_constraint( diag( 1, ncol(r), ncol(r) ), rep(">=", ncol(r)), rep(0, ncol(r)) )
 
 ## Specify minimum variance problem object
-op <- OP( objective = Q_objective(S), constraints = c(full_invest, long_only) )
+MV <- OP( objective = Q_objective(S), constraints = c(full_invest, long_only) )
 
-res <- ROI_solve( op, solver = "quadprog" )
+res <- ROI_solve( MV, solver = "quadprog" )
 names( res$solution ) <- colnames( US30 )
 
 ## optimal weights
@@ -246,7 +280,6 @@ points( x = mv[1], y = mv[2], col = "green" )
 text( x = mv[1], y = mv[2], labels = "MV", pos = 3, col = "green" )
 
 ## this function samples for given asset properties n random portfolios in risk/return space
-
 make_random_portfolios <- function( MU, COV, n ){
     u <-  matrix( runif(n * length(MU)), nrow = n )
     constituents <- sample( 1:length(MU), n, replace = TRUE )
@@ -266,6 +299,10 @@ plot( sigma, mu, xlim = c(0, max(sigma)*1.1), ylim = c(0, max(mu)*1.1),
 points( cbind( rpf[["sd"]], rpf[["mu"]] ), col = "lightgrey" )
 points( sigma, mu )
 text( x = sigma, y = mu, labels = colnames(US30), pos = 3 )
+points( x = ew[1], y = ew[2], col = "orange" )
+text( x = ew[1], y = ew[2], labels = "EW", pos = 3, col = "orange" )
+points( x = mv[1], y = mv[2], col = "green" )
+text( x = mv[1], y = mv[2], labels = "MV", pos = 3, col = "green" )
 
 
 ## Example 7)
@@ -274,8 +311,9 @@ text( x = sigma, y = mu, labels = colnames(US30), pos = 3 )
 ## Say we like the return of the EW protfolio, but with minimal risk
 target_return <- L_constraint( mu, ">=", ew[2] )
 
-constraints(op)
-constraints(op) <- rbind( constraints(op),  )
+constraints(MV)
+op <- MV
+constraints(op) <- rbind( constraints(MV), target_return )
 
 res <- ROI_solve( op, solver = "quadprog" )
 names( res$solution ) <- colnames( US30 )
@@ -285,7 +323,7 @@ round( res$solution*100, 2 )
 mvtr <- c( sqrt(res$solution %*% S %*% res$solution),
         res$solution %*% mu )
 points( x = mvtr[1], y = mvtr[2], col = "blue" )
-text( x = mvtr[1], y = mvtr[2], labels = "MinVar_tr", pos = 3, col = "blue" )
+text( x = mvtr[1], y = mvtr[2], labels = "MeanVar", pos = 3, col = "blue" )
 
 
 ## Example 8)
@@ -294,9 +332,7 @@ text( x = mvtr[1], y = mvtr[2], labels = "MinVar_tr", pos = 3, col = "blue" )
 grid_start <- mv[2]
 grid_end <- mu[ which.max(mu) ]
 grid_points <- 1000
-
 grid <- seq( from = grid_start, to = grid_end, length.out = 1000 )
-MV <- OP( objective = Q_objective(S), constraints = c(full_invest, long_only) )
 
 l <- lapply( grid, function(r) {
     target_return <- L_constraint( mu, ">=", r )
@@ -314,11 +350,11 @@ lines( efficient_frontier, col = "red" )
 ## Integer Programming: Constructing an Index Fund (Cornuejols and Tuetuencue, 2008)
 
 ## We use approx 60 months for calibration and 12 months for
-## testing. We take only those stocks into account which where included
-## in the US30 or US500 index at all time from early 2008 to the end of 2013
+## testing. We take only stocks into account, which where included
+## in the US30 or US500 index at all time from early 2008 to the end of 2013.
 #us_large <- na.locf( US500["20080101::"], maxgap = 2 ) # too large for dense representation
 #us_large <- us_large[, !apply(us_large, 2, function(x) any(is.na(x))) ]
-r <- na.omit( ROC( US30[ endpoints(index(US30["::20121231"]), on = "weeks") ], type = "discrete" ) )
+r <- na.omit( ROC(US30[endpoints(index(US30["::20121231"]), on = "weeks")], type = "discrete" ) )
 
 ## we'd like to choose 15 stocks to replicate the index
 q <- 15
@@ -344,7 +380,7 @@ for( i in 1:n ){
 }
 sum_most_similar <- L_constraint( full_block, rep("==", n), rep(1, n) )
 
-block_x <- diag(1, length(rho))
+block_x <- diag( 1, length(rho) )
 block_y <- matrix( rep(diag( -1,  n ), n ), ncol = n, byrow = TRUE )
 x_ij_constraints <- L_constraint( cbind(block_x, block_y),
                                   rep("<=", nrow(block_x)),
@@ -363,6 +399,7 @@ names( idx_included ) <- colnames( r )
 
 idx_included
 
+
 ## NOTE: we should really use market capitalization instead
 
 ## calculate weights of assets in index fund
@@ -377,11 +414,18 @@ bm_w <- as.numeric(last(US30["::20121231"]))/sum(as.numeric(last(US30["::2012123
 # compare weights
 cbind( bm_w, fund_w)
 
-## out of sample performance
-
+## approx out of sample performance comparison
 ## Q1 perf comparison bm vs fund
 last( US30["::20130331"] ) %*% fund_w / (last( US30["::20130331"] ) %*% bm_w)
 ## Q2 perf comparison bm vs fund
 last( US30["::20130630"] ) %*% fund_w / (last( US30["::20130630"] ) %*% bm_w)
 ## 1Y perf comparison bm vs fund
 last( US30 ) %*% fund_w / (last( US30 ) %*% bm_w)
+
+## plot index fund equity line (indexed at 2012-01-01)
+os <- US30["20120101::"]
+fund_el <- xts( apply(coredata(os) , 1, function(x) x%*% fund_w ), order.by = index(os) )
+bm_el <- xts( apply(coredata(os) , 1, function(x) x%*% bm_w ), order.by = index(os) )
+
+require( "PerformanceAnalytics" )
+chart.CumReturns( cbind(FUND = ROC(fund_el, type = "discrete"), BM = ROC(bm_el, type = "discrete")), legend = "topleft" )
