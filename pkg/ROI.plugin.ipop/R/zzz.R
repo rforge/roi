15b6043798b26341a7151084c00651a9d3f566be
fschwendinger <fschwendinger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-25 13:31:07 +0000

    add signature to plugin
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roi@133 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ROI.plugin.ipop/R/zzz.R
+++ pkg/ROI.plugin.ipop/R/zzz.R
@@ -1,14 +1,32 @@
+make_QP_signatures <- function() {
+    .ROI_plugin_make_signature( objective = "Q",
+                        constraints = c("X", "L"),
+                        types = c("C"),
+                        bounds = c("X", "V"),
+                        cones = c("free"),
+                        maximum = c(TRUE, FALSE) )
+}
+
+make_LP_signatures <- function() {
+    .ROI_plugin_make_signature( objective = "L",
+                        constraints = c("X", "L"),
+                        types = c("C"),
+                        bounds = c("X", "V"),
+                        cones = c("free"),
+                        maximum = c(TRUE, FALSE) )
+}
+
 .onLoad <- function( libname, pkgname ) {
     ## Solver plugin name (based on package name)
     if( ! pkgname %in% ROI_registered_solvers() ){
         ## Register solver methods here.
         ## One can assign several signatures a single solver method
         solver <- .ROI_plugin_get_solver_name( pkgname )
-        .ROI_plugin_register_solver_method( signatures = .ROI_plugin_make_QP_signatures(),
+        .ROI_plugin_register_solver_method( signatures = make_QP_signatures(),
                                           solver = solver,
                                           method =
             getFunction( "solve_QP", where = getNamespace(pkgname)) )
-        .ROI_plugin_register_solver_method( signatures = .ROI_plugin_make_LP_signatures(),
+        .ROI_plugin_register_solver_method( signatures = make_LP_signatures(),
                                             solver = solver,
                                             method =
             getFunction( "solve_LP", where = getNamespace(pkgname)) )
