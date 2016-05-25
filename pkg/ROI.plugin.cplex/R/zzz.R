f76efee0e945aa7be63b6761da556f543a158ef5
fschwendinger <fschwendinger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-25 13:38:57 +0000

    add signature to plugin
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roi@140 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ROI.plugin.cplex/R/zzz.R
+++ pkg/ROI.plugin.cplex/R/zzz.R
@@ -1,10 +1,19 @@
+make_MIQCP_signatures <- function() {
+    .ROI_plugin_make_signature( objective = c("L", "Q"),
+                        constraints = c("X", "L", "Q"),
+                        types = c("C", "I", "B", "CI", "CB", "IB", "CIB"),
+                        bounds = c("X", "V"),
+                        cones = c("free"),
+                        maximum = c(TRUE, FALSE) )
+}
+
 .onLoad <- function( libname, pkgname ) {
     ## Solver plugin name (based on package name)
     if( ! pkgname %in% ROI_registered_solvers() ){
         ## Register solver methods here.
         ## One can assign several signatures a single solver method
         solver <- .ROI_plugin_get_solver_name( pkgname )
-        .ROI_plugin_register_solver_method( signatures = .ROI_plugin_make_MIQCP_signatures(),
+        .ROI_plugin_register_solver_method( signatures = make_MIQCP_signatures(),
                                             solver = solver,
                                             method =
             getFunction( "solve_OP", where = getNamespace(pkgname)) )
