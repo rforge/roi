445773f92f2501a0fac99fe4cac56b25f5654241
fschwendinger <fschwendinger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-25 13:33:44 +0000

    add signature to plugin
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roi@134 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ROI.plugin.nloptr/R/zzz.R
+++ pkg/ROI.plugin.nloptr/R/zzz.R
@@ -2,12 +2,12 @@
 ## based on nloptr interface
 
 ROI_make_NLP_FXCV_signatures <- function()
-    ROI_make_signature( objective = c("F"),
-                        constraints = c("X", "F"),
-                        types = c("C"),
-                        bounds = c("X", "V"),
-                        cones = c("free"),
-                        maximum = c(TRUE, FALSE) )
+    .ROI_plugin_make_signature( objective = c("F"),
+                                constraints = c("X", "F"),
+                                types = c("C"),
+                                bounds = c("X", "V"),
+                                cones = c("free"),
+                                maximum = c(TRUE, FALSE) )
 
 .onLoad <- function( libname, pkgname ) {
     ## Solver plugin name (based on package name)
