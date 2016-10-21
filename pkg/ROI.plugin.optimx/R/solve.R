e74e7bd2326e8d447727b1b3685529df95b8cafa
fschwendinger <fschwendinger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-10-21 20:36:08 +0000

    add ROI.plugin.optimx
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roi@232 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/ROI.plugin.optimx/R/solve.R
@@ -0,0 +1,71 @@
+## get_lb
+## ======
+##
+## get lower bound constraints
+get_lb <- function(x) {
+    if( !length(bounds(x)$lower$val) ) {
+        lb <- 0
+    } else {
+        lb <- numeric( length(x$objective) )
+        lb[ bounds(x)$lower$ind ] <- bounds(x)$lower$val
+    }
+    return(lb)
+}
+
+## get_ub
+## ======
+##
+## get upper bound constraints
+get_ub <- function(x) {
+    if( !length(bounds(x)$upper$val) ) {
+        ub <- Inf
+    } else {
+        ub <- rep.int(Inf, length(x$objective))
+        ub[ bounds(x)$upper$ind ] <- bounds(x)$upper$val
+    }
+    return(ub)
+}
+
+solve_optimx <- function( x, control ) {
+    solver <- .ROI_plugin_get_solver_name( getPackageName() )
+
+    if ( is.null(control$par) )
+        stop("argument 'start' is missing with no default")
+
+    lb <- get_lb(x)
+    ub <- get_ub(x)
+
+    opti <- list(optimx)
+    opti$par <- control$par
+    opti$fn <- terms(objective(x))$F
+    opti$gr <- G(objective(x))
+    opti$hess <- control$hess
+    opti$lower <- lb
+    opti$upper <- ub
+    opti$method <- control$method
+    opti$itnmax <- control$itnmax    
+    opti$hessian <- if ( is.null(control$hessian) ) FALSE else control$hessian
+    if ( is.null(control$method) ) control$all.methods <- TRUE
+    cn <- setdiff(names(control), c("par", "hess", "method", "itnmax", "hessian"))
+    control$maximize <- x$maximum
+    opti$control <- control[cn]
+    mode(opti) <- "call"
+
+    if ( isTRUE(control$dry_run) )
+        return( opti )
+
+    out <- eval(opti)
+
+    i <- if ( x$maximum ) which.max(out$value) else which.min(out$value)
+
+    solution <- as.numeric(out[i, seq_len(length(control$par))])
+    algorithm <- if ( is.null(control$method) ) "all" else control$method
+
+    .ROI_plugin_canonicalize_solution(  solution  = solution,
+                                        optimum   = out$value[i],
+                                        status    = out$convcode[i],
+                                        solver    = solver,
+                                        message   = out,
+                                        algorithm = rownames(out)[i])
+}
+
---
+++ pkg/ROI.plugin.optimx/Work/Test.R
@@ -0,0 +1,53 @@
+q("no")
+R
+
+library(ROI)
+
+## Rosenbrock Banana objective function
+## solution: c(1, 1)
+f <- function(x) {
+    return( 100 * (x[2] - x[1] * x[1])^2 + (1 - x[1])^2 )
+}
+
+f.grad <- function(x) {
+    return( c( -400 * x[1] * (x[2] - x[1] * x[1]) - 2 * (1 - x[1]),
+               200 * (x[2] - x[1] * x[1])) )
+}
+
+## lower and upper bounds
+lb <- c( -3, -3 )
+ub <- c(  3,  3 )
+
+VB <- function(lower, upper) {
+    li <- which(lower != 0)
+    ui <- which(upper != Inf)
+    return(V_bound(li=li, ui=ui, lb=lower[li], ub=upper[ui], nobj=length(lower)))
+}
+
+
+x <- OP(F_objective(f, 2L, f.grad), bounds=VB(lb, ub))
+control <- list(start = c(-1.2, 1), method = "L-BFGS-B")
+# Solve Rosenbrock Banana function.
+res <- ROI_solve(x, solver="optimx", control)
+res
+solution(res)
+
+
+x <- OP(F_objective(f, 2L), bounds=VB(lb, ub))
+control <- list(start = c(-1.2, 1), method = "L-BFGS-B")
+# Solve Rosenbrock Banana function.
+res <- ROI_solve(x, solver="optimx", control)
+res
+solution(res)
+
+
+x <- OP(F_objective(f, 2L, f.grad), bounds=VB(lb, ub))
+control <- list(start = c(-1.2, 1))
+# Solve Rosenbrock Banana function.
+res <- ROI_solve(x, solver="optimx", control)
+res
+attributes(res)$meta
+solution(res)
+solution(res, "msg")
+
+
