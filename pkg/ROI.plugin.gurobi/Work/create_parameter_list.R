5c5b60cd5beaa4d249aa2c423b02ca88ec4885d2
fschwendinger <fschwendinger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-02-21 13:30:19 +0000

    rm work folder
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roi@289 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ROI.plugin.gurobi/Work/create_parameter_list.R
+++
@@ -1,7 +0,0 @@
-
-x <- read.table("Gurobi_Parameters.csv", sep=";")
-
-tail(x)
-y <- sprintf('    .ROI_plugin_register_solver_control( solver, "%s", "X" )    ## %s', x[,1], x[,2])
-writeLines(y, "solver_controls.R")
-
