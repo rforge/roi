dbb43a471d3b2b9fa9f476cf498d2dea83d825b6
fschwendinger <fschwendinger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-05-17 12:01:15 +0000

    update db
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roi@353 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/db/create_solver_db.R
+++ www/db/create_solver_db.R
@@ -1,4 +1,6 @@
 
+setwd("/home/florian/work/Optimization/ROI/ROI_R-Forge/www/db")
+
 CRAN <- "https://cran.r-project.org/"
 RFORGE <- "http://r-forge.r-project.org/"
 
@@ -60,5 +62,54 @@ plugins_rforge2 <- parse_descriptions(plugins_rforge_not_compiled)
 as.df <- function(x) as.data.frame(x, stringsAsFactors = FALSE)
 
 plugins <- rbind(as.df(plugins_cran), as.df(plugins_rforge), plugins_rforge2)
+
+library(ROI)
+
+pkgs <- gsub("ROI.plugin.", "", plugins$Package, fixed = TRUE)
+
+pkgs
+
+sign <- make_MILP_signatures()
+class(sign)
+signature_entries <- colnames(sign)
+
+entries <- ROI:::solver_db$get_entries()
+
+extract_signature <- function(x) {
+    as.df(x[c("solver", signature_entries)])
+}
+
+solver_signatures <- do.call(rbind, lapply(entries, extract_signature))
+rownames(solver_signatures) <- NULL
+
+fun <- function(solver) solver_signatures[solver_signatures$solver == solver, -1L]
+usolver <- unique(solver_signatures$solver)
+solver_signatures <- setNames(lapply(usolver, fun), usolver)
+
+str(solver_signatures)
+
+head(plugins)
+
+i <- 1L
+
+plugins$Signature <- list(NA)
+for ( i in seq_len(nrow(plugins)) ) {
+    solver_name <- gsub("ROI.plugin.", "", plugins$Package[i], fixed = TRUE)
+
+    if ( solver_name %in% names(solver_signatures) ) {
+        solver_signature <- solver_signatures[[solver_name]]
+        rownames(solver_signature) <- NULL
+        plugins$Signature[[i]] <- solver_signature
+    } else {
+        cat(solver_name, "not available!\n")
+    }
+}
+
+head(plugins$Signature)
+plugins$Signature[[1L]]
+plugins$Signature[[2L]]
+head(plugins$Signature[[3L]])
+
+getwd()
 
 saveRDS(plugins, "SOLVERS.rds")
