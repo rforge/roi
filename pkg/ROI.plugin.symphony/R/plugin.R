d4172d79c582d928c847b20878ecf823a9737d3b
stefan7th <stefan7th@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-05-17 08:34:09 +0000

    status codes updated according to SYMPHONY 5.6.16 headers
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roi@349 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ROI.plugin.symphony/R/plugin.R
+++ pkg/ROI.plugin.symphony/R/plugin.R
@@ -107,54 +107,77 @@ solve_OP <- function( x, control ){
                                 229L,
                                 "TM_NODE_LIMIT_EXCEEDED",
                                 "TM_NODE_LIMIT_EXCEEDED"
-                          )
+                                     )
+    ## updated status codes as seen in https://github.com/coin-or/SYMPHONY/blob/master/SYMPHONY/include/symphony.h
+    ## SYMPHONY version 5.6.16. thanks to Ricardo Sanchez
     ROI_plugin_add_status_code_to_db(solver,
                                 230L,
+                                "TM_ITERATION_LIMIT_EXCEEDED",
+                                "TM_ITERATION_LIMIT_EXCEEDED"
+                                )
+    ROI_plugin_add_status_code_to_db(solver,
+                                231L,
                                 "TM_TARGET_GAP_ACHIEVED",
                                 "TM_TARGET_GAP_ACHIEVED"
                                 )
     ROI_plugin_add_status_code_to_db(solver,
-                                231L,
+                                232L,
                                 "TM_FOUND_FIRST_FEASIBLE",
                                 "TM_FOUND_FIRST_FEASIBLE"
                                 )
     ROI_plugin_add_status_code_to_db(solver,
-                                232L,
+                                233L,
                                 "TM_FINISHED",
                                 "TM_FINISHED"
                                 )
     ROI_plugin_add_status_code_to_db(solver,
-                                233L,
+                                234L,
                                 "TM_UNFINISHED",
                                 "TM_UNFINISHED"
                                 )
     ROI_plugin_add_status_code_to_db(solver,
-                                240L,
+                                235L,
                                 "TM_FEASIBLE_SOLUTION_FOUND",
                                 "TM_FEASIBLE_SOLUTION_FOUND"
                                 )
     ROI_plugin_add_status_code_to_db(solver,
-                                235L,
+                                236L,
                                 "TM_SIGNAL_CAUGHT",
                                 "TM_SIGNAL_CAUGHT"
                                 )
+   ROI_plugin_add_status_code_to_db(solver,
+                                237L,
+                                "TM_UNBOUNDED",
+                                "TM_UNBOUNDED"
+                                )
     ROI_plugin_add_status_code_to_db(solver,
-                                -251L,
+                                238L,
+                                "PREP_OPTIMAL_SOLUTION_FOUND",
+                                "PREP_OPTIMAL_SOLUTION_FOUND",
+                                0L
+                                )
+    ROI_plugin_add_status_code_to_db(solver,
+                                239L,
+                                "PREP_NO_SOLUTION",
+                                "PREP_NO_SOLUTION"
+                                )
+    ROI_plugin_add_status_code_to_db(solver,
+                                -250L,
                                 "TM_ERROR__NO_BRANCHING_CANDIDATE",
                                 "TM_ERROR__NO_BRANCHING_CANDIDATE"
                                 )
     ROI_plugin_add_status_code_to_db(solver,
-                                -252L,
+                                -251L,
                                 "TM_ERROR__ILLEGAL_RETURN_CODE",
                                 "TM_ERROR__ILLEGAL_RETURN_CODE"
                                 )
     ROI_plugin_add_status_code_to_db(solver,
-                                -253L,
+                                -252L,
                                 "TM_ERROR__NUMERICAL_INSTABILITY",
                                 "TM_ERROR__NUMERICAL_INSTABILITY"
                                 )
     ROI_plugin_add_status_code_to_db(solver,
-                                -254L,
+                                -253L,
                                 "TM_ERROR__COMM_ERROR",
                                 "TM_ERROR__COMM_ERROR"
                                 )
@@ -162,6 +185,11 @@ solve_OP <- function( x, control ){
                                 -275L,
                                 "TM_ERROR__USER",
                                 "TM_ERROR__USER"
+                                )
+    ROI_plugin_add_status_code_to_db(solver,
+                                -276L,
+                                "PREP_ERROR",
+                                "PREP_ERROR"
                                 )
     invisible(TRUE)
 }
