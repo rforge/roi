2a549d870de7843d27c62e039247e1cfb06c2efd
fschwendinger <fschwendinger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-05-18 14:37:00 +0000

    added multisolutions tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roi@378 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ROI.plugin.lpsolve/tests/test_lp_solve.R
+++ pkg/ROI.plugin.lpsolve/tests/test_lp_solve.R
@@ -1,3 +1,4 @@
+library(slam)
 library(ROI)
 library(ROI.plugin.lpsolve)
 
@@ -121,6 +122,53 @@ test_milp_02 <- function(solver) {
     opt <- ROI_solve(x, solver=solver)
     check("MILP-02@01", all(A %*% opt$solution <= b))
     check("MILP-02@01", equal(opt$solution , c(5, 2.75, 3), tol=1e-01))
+}
+
+## LP - Multiple Solutions
+## solver <- "msbinlp"
+test_blp_multisol_01 <- function(solver) {
+    x <- OP(objective = c(1, 1),
+             constraints = L_constraint(c(1, 1), "==", 1),
+             types = c("B", "B"))
+
+    opt <- ROI_solve(x, solver = solver, nsol_max = 2L)
+    
+    sol <- do.call(rbind, solution(opt))
+    check("BLPMS-01@01", equal(nrow(sol), 2L))
+    check("BLPMS-01@02", equal(ncol(sol), 2L))
+    check("BLPMS-01@03", (equal(sol, diag(2)) | equal(sol, diag(2)[2:1, ])))
+    sol
+}
+
+## -----------------------------------------------------------------------------
+##
+## Pruning Moves [Matteo Fischetti, Domenico Salvagnin] (2009)
+##
+## -----------------------------------------------------------------------------
+## min   - x_1 - x_2 - x_3 - x_4 - 99 x_5
+## s.t.  x_1 + x_2 <= 1
+##       x_3 + x_4 <= 1
+##       x_4 + x_5 <= 1
+##       x_i in {0, 1}
+test_blp_multisol_02 <- function(solver) {
+    x <- OP()
+    objective(x) <- L_objective(c(-1, -1, -1, -1, -99))
+    mat <- simple_triplet_matrix(rep(1:3, 2), 
+                                 c(c(1, 3, 4), c(2, 4, 5)), 
+                                 rep(1, 6))
+    constraints(x) <- L_constraint(mat, 
+                                   dir = leq(3), 
+                                   rhs = rep.int(1, 3))
+    types(x) <- rep("B", length(x))
+
+    opt <- ROI_solve(x, solver = solver, nsol_max = 2L)
+
+    sol <- do.call(rbind, solution(opt))
+    y <- rbind(c(0, 1, 1, 0, 1), c(1, 0, 1, 0, 1))
+    check("BLPMS-02@01", equal(nrow(sol), 2L))
+    check("BLPMS-02@02", equal(ncol(sol), 5L))
+    check("BLPMS-02@03", (equal(sol, y) | equal(sol, y[2:1, ])))
+    sol
 }
 
 if ( !any("lpsolve" %in% names(ROI_registered_solvers())) ) {
