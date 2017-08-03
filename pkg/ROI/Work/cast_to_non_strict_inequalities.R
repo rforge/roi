1d996774cc4af0688ed37e9ce9956a2f2060f619
stefan7th <stefan7th@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-08-03 12:13:46 +0000

    scalar case example added and caster improved
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roi@415 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ROI/Work/cast_to_non_strict_inequalities.R
+++ pkg/ROI/Work/cast_to_non_strict_inequalities.R
@@ -80,8 +80,9 @@ cast_to_non_strict_inequalities.L_constraint <- function( x, i ){
             if( any(idx_cast) ){
                 coefs <- row_sums( x$L[row_valid,][idx_cast,] )
                 x$dir[row_valid][idx_cast] <- ifelse( coefs >= 0, x$dir[row_valid][idx_cast], c(">" = "<", "<" = ">")[x$dir[row_valid][idx_cast]] )
-                x$L[row_valid,][idx_cast,]$v <- 1
-                x$rhs[row_valid][idx_cast] <- x$rhs[row_valid][idx_cast]/coefs
+                x$L$v[ x$L$i == which(row_valid)[idx_cast] ] <- 1
+                eps <- c(">" = 1, "<" = -1)[x$dir[row_valid][idx_cast]]
+                x$rhs[row_valid][idx_cast] <- x$rhs[row_valid][idx_cast]/coefs + eps
                 x$dir[row_valid][idx_cast] <- sense_db[ x$dir[row_valid][idx_cast] ]
             } else {
                 warning( "some constraints cannot be casted to non-strict inequalities" )
@@ -91,6 +92,69 @@ cast_to_non_strict_inequalities.L_constraint <- function( x, i ){
     ## FIXME: convert all remaining row senses to non-strict inequalities here?
     x
 }
+
+op_nonstrict <- op
+
+constraints(op_nonstrict) <- cast_to_non_strict_inequalities( constraints(op), which(types(op) == "I") )
+
+## run optimizations (as will be implemented)
+for(solver in milpsolvers){
+  timing <- system.time(res <- ROI_solve(op_nonstrict, solver = solver))["elapsed"]
+  milp_results[solver, ] <- c(res$objval, timing)
+  sol[solver, ] <- solution(res)
+}
+
+milp_results
+sol
+
+## check constraints against original formulation -> OK
+g <- rowapply_simple_triplet_matrix( constraints(op)$L, function(x) x %*% sol[1,] )
+sapply( seq_along(g), function(i) getFunction(constraints(op)$dir[i])(g[i], constraints(op)$rhs[i]) )
+
+
+## Example: scalar case
+
+## Simple mixed integer linear program.
+## maximize:    3 x_1 + 11 x_2 + 6 x_3
+## subject to: -1 x_1 - 2 x_2 +   x_3 <= 4
+##                x_1 + 4 x_2 -1/4x_3 <= 2
+##                             1/2x_3 < 3
+##                x_1, x_3 are non-negative integers
+##                x_2 is a non-negative real number
+
+## initialize ROI optimization problem
+op <- OP( objective = c(3, 11, 6),
+          constraints = L_constraint(L = matrix(c(-1, 1, 0, -2, 4, 0,
+                                                  1, -1/4, 1/2), nrow = 3),
+                                     dir = c("<=", "<=", "<"),
+                                     rhs = c(4, 2, 3)),
+          types = c("I", "C", "I"),
+          maximum = TRUE )
+
+## my installed milp solvers ...
+milpsolvers <- names( ROI:::get_solver_methods(ROI:::OP_signature(op)) )
+
+## initialize results objects
+milp_results <- data.frame(objval = rep(NA, length.out = length(milpsolvers)),
+                           timing = NA)
+rownames(milp_results) <- milpsolvers
+sol <- matrix( NA, ncol = length(objective(op)), nrow = length(milpsolvers) )
+rownames( sol) <- milpsolvers
+
+## run optimizations (as currently implemented)
+for(solver in milpsolvers){
+  timing <- system.time(res <- ROI_solve(op, solver = solver))["elapsed"]
+  milp_results[solver, ] <- c(res$objval, timing)
+  sol[solver, ] <- solution(res)
+}
+
+milp_results
+sol
+
+## check constraints
+g <- rowapply_simple_triplet_matrix( constraints(op)$L, function(x) x %*% sol[1,] )
+sapply( seq_along(g), function(i) getFunction(constraints(op)$dir[i])(g[i], constraints(op)$rhs[i]) )
+
 
 op_nonstrict <- op
 
