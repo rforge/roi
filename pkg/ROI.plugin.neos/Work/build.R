67a472dfa0ea981e33cadbe02028f4deecfda135
fschwendinger <fschwendinger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-10-31 10:38:09 +0000

    add README
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roi@473 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/ROI.plugin.neos/Work/build.R
@@ -0,0 +1,11 @@
+
+## knitr:::knit2html("README.Rmd")
+## system("pandoc README.md -o README.html")
+
+rmarkdown:::render("README.Rmd")
+
+## args(knitr:::render_markdown)
+## knitr:::render_markdown("README.Rmd")
+## 
+## library(knitr)
+## ls("package:knitr")
