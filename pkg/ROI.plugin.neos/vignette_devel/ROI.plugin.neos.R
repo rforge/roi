d40b0cc42665ce6b7130df3841d47004f45808e9
fschwendinger <fschwendinger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-06-11 10:50:59 +0000

    update vignette
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/roi@499 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ROI.plugin.neos/vignette_devel/ROI.plugin.neos.R
+++ pkg/ROI.plugin.neos/vignette_devel/ROI.plugin.neos.R
@@ -1,8 +1,8 @@
-### R code from vignette source '/home/florian/work/Optimization/ROI/ROI_R-Forge/pkg/ROI.plugin.neos/vignettes_devel/ROI.plugin.neos.Rnw'
+### R code from vignette source '/home/florian/work/Optimization/ROI/ROI_R-Forge/pkg/ROI.plugin.neos/vignette_devel/ROI.plugin.neos.Rnw'
 ### Encoding: UTF-8
 
 ###################################################
-### code chunk number 1: ROI.plugin.neos.Rnw:29-31
+### code chunk number 1: ROI.plugin.neos.Rnw:27-29
 ###################################################
 library(knitr)
 opts_chunk$set(tidy=FALSE, fig.width=8, fig.height=4)
